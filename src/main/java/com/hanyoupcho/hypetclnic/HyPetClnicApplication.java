42c67a1bfbc8026b4058ce08198bf945bf387b9a
Hanyoup Cho <hanyoup.cho@gmail.com>
2018-10-21 19:28:17 +0900

    Initial Project Add

---
+++ src/main/java/com/hanyoupcho/hypetclnic/HyPetClnicApplication.java
@@ -0,0 +1,12 @@
+package com.hanyoupcho.hypetclnic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyPetClnicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HyPetClnicApplication.class, args);
+    }
+}
